1c1ebd415353bb3bcdf44fb17d0d8d492ec205fc
Daria Malenko <malenkodaria.99@gmail.com>
2025-04-26 13:55:27 +0200

    Deploy to GitHub pages is added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: isProd ? process.env.PAGES_BASE_PATH : '',
 };
 
 export default nextConfig;
